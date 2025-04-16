772e8575ccf0e163f5368beeac04be6f183067ce
orianadecaro <orianadecaro@gmail.com>
2025-04-16 19:19:00 -0300

    change label

--- src/page/admin/book/index.jsx
+++ src/page/admin/book/index.jsx
@@ -89,9 +89,7 @@ const BookList = () => {
         {isModalOpen && (
           <div className="fixed inset-0 z-50 bg-[rgba(0,0,0,0.4)]  flex items-center justify-center">
             <div className="bg-white rounded-lg p-6 w-full max-w-md">
-              <h2 className="text-lg font-semibold mb-4">
-                Agregar prestamo o devolucion
-              </h2>
+              <h2 className="text-lg font-semibold mb-4">Agregar Libro</h2>
               <form onSubmit={handleSubmit} className="space-y-3">
                 <div>
                   <label className="text-sm font-medium">Titulo</label>
